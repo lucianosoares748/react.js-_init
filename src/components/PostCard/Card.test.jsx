5d511e20ad1a6f16afdd6fa54f21159e69f89baf
luciano <lucianosoaresdev748@gmail.com>
2022-11-03 11:21:50 -0300

    test mock

---
+++ src/components/PostCard/Card.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { PostCard } from ".";
+import { postCardMock } from "./mock";
+
+const props = postCardMock;
+
+describe("<PostCard />", () => {
+  it("should render PostCard correctly", () => {
+    render(<PostCard {...props} />);
+
+    expect(
+      screen.getByRole("heading", { name: /title 1/i })
+    ).toBeInTheDocument();
+    expect(screen.getByText("body1")).toBeInTheDocument();
+    expect(screen.getByAltText("title 1")).toBeInTheDocument();
+  });
+});
---
+++ src/components/PostCard/mock.js
@@ -0,0 +1,7 @@
+export const postCardMock = {
+  title: "title 1",
+  body: "body1",
+  id: "1",
+  cover: "img/img.png",
+  alt: "minha photo",
+};
