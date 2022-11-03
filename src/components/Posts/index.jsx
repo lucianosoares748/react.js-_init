9a86431a86bc8c567e565cdd5fcdb49e99d0bcac
luciano <lucianosoaresdev748@gmail.com>
2022-11-03 16:02:28 -0300

    test component posts

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,6 +1,8 @@
-import { PostCard } from "../PostCard";
 import "./style.css";
-export const Posts = ({ posts }) => (
+
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts = [] }) => (
   <div className="posts">
     {posts.map((post) => (
       <PostCard
